b4e4d93003b3d8579eafa7f330246938e04ad4b9
Sam Pupke <sampupke@gmail.com>
2024-01-23 09:35:58 -0500

    Uploaded first lab starting code
    Linked List Based Stacks & Queues lab

---
+++ Lab Work/Linked List Based Stacks and Queues/T22.cpp
@@ -0,0 +1,21 @@
+#include "ListQueue.h"
+
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    ListQueue<string> queue1;
+    for (int i=0; i<13; i++){
+        queue1.enqueue("frankline");
+    }
+    queue1.dequeue();
+
+    if (queue1.size() != 12)
+    {
+        cout << "expected/correct value 12, actual value when testing " << queue1.size() << ".\n";
+        return 1;
+    }
+    cout << "Passed" << endl;
+    return 0;
+}
