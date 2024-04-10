fd10bdf624ec9856e3e1e31f975a4ebdd254a277
Sam Pupke <sampupke@gmail.com>
2024-04-10 15:11:38 -0400

    update to magic squares
    int to unsigned long long, generator for doubly even squares, and writing to files

--- Review Work/strachey.cpp
+++ Review Work/strachey.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
-//To shuffle:
-//https://en.wikipedia.org/wiki/Magic_square#Transformations_that_preserve_the_magic_property
+// To shuffle:
+// https://en.wikipedia.org/wiki/Magic_square#Transformations_that_preserve_the_magic_property
 
-//k is the safety precursor to making even sized magic squares. 
-//even square dimensions are determined by 4k+2
+// k is the safety precursor to making even sized magic squares.
+// even square dimensions are determined by 4k+2
 //@pre k >= 1
-const int k = 1;
-//j is the safety precursor for stepping in a strachey alg square
+const unsigned long long k = 1;
+// j is the safety precursor for stepping in a strachey alg square
 //@pre j>=0
-const int j = 1;
+const unsigned long long j = 1;
 
-//n is the safety precursor to making odd sized magic squares.
-//odd square dimensions are determined by n
+// n is the safety precursor to making odd sized magic squares.
+// odd square dimensions are determined by n
 //@pre n >= 3 && n % 2 == 1
-const int n = 3;
-//s is the safety precursor for odd sized magic square starting values
-//any nonzero positive integer is allowed
+const unsigned long long n = 3;
+// s is the safety precursor for odd sized magic square starting values
+// any nonzero positive integer is allowed
 //@pre s >= 1
-const int s = 1;
-//d is the safety precursor for stepping in a siamese square
+const unsigned long long s = 1;
+// d is the safety precursor for stepping in a siamese square
 //@pre d>=0
-const int d = j;
-
+const unsigned long long d = j;
 
 // Function to print the magic square and its row, column, and diagonal sums
-void printMagicSquare(const vector<vector<int>> &magicSquare, int size)
+void printMagicSquare(const vector<vector<unsigned long long>> &magicSquare, unsigned long long size)
 {
-    vector<int> rowSum(size, 0);
-    vector<int> colSum(size, 0);
-    int mainDiagSum = 0;
-    int antiDiagSum = 0;
+    vector<unsigned long long> rowSum(size, 0);
+    vector<unsigned long long> colSum(size, 0);
+    unsigned long long mainDiagSum = 0;
+    unsigned long long antiDiagSum = 0;
 
     cout << "Magic Square:" << endl;
-    for (int i = 0; i < size; ++i)
+    for (unsigned long long i = 0; i < size; ++i)
     {
-        for (int j = 0; j < size; ++j)
+        for (unsigned long long j = 0; j < size; ++j)
         {
             cout << magicSquare[i][j] << "\t";
             rowSum[i] += magicSquare[i][j];
@@ -56,90 +56,94 @@ void printMagicSquare(const vector<vector<int>> &magicSquare, int size)
     }
 
     // Print column sums
-    for (int j = 0; j < size; ++j)
+    for (unsigned long long j = 0; j < size; ++j)
     {
         cout << "--------";
     }
-    cout<<endl;
-    for (int j = 0; j < size; ++j)
+    cout << endl;
+    for (unsigned long long j = 0; j < size; ++j)
     {
         cout << colSum[j] << "\t";
     }
-    cout << "\\"<<mainDiagSum<<" /"<<antiDiagSum<<endl;
-}
-
-//Function to generate the next value in a siamese square. 
-//Output must return a nonzero, and reliant on @param num
-int siameseSequence(int num, int step) {
-    return num+step;
+    cout << "\\" << mainDiagSum << " /" << antiDiagSum << endl;
 }
 
 // Function to generate Siamese magic square of odd order
-vector<vector<int>> generateSiameseMagicSquare(int startNum, int size, int step) {
-    vector<vector<int>> siameseMagicSquare(size, vector<int>(size, 0));
+vector<vector<unsigned long long>> generateSiameseMagicSquare(unsigned long long startNum, unsigned long long size, unsigned long long step)
+{
+    vector<vector<unsigned long long>> siameseMagicSquare(size, vector<unsigned long long>(size, 0));
 
-    int num = startNum;
-    int row = 0;
-    int col = size / 2;
+    unsigned long long num = startNum;
+    unsigned long long row = 0;
+    unsigned long long col = size / 2;
 
-    for (int i = 0; i < size * size; ++i) {
+    for (unsigned long long i = 0; i < size * size; ++i)
+    {
         siameseMagicSquare[row][col] = num;
 
         // Move diagonally up and right
-        int nextRow = (row - 1 + size) % size;
-        int nextCol = (col + 1) % size;
+        unsigned long long nextRow = (row - 1 + size) % size;
+        unsigned long long nextCol = (col + 1) % size;
 
         // Check if the next position is filled
-        if (siameseMagicSquare[nextRow][nextCol] != 0) {
+        if (siameseMagicSquare[nextRow][nextCol] != 0)
+        {
             // Move vertically down one box instead
             row = (row + 1) % size;
-        } else {
+        }
+        else
+        {
             // Move diagonally up and right
             row = nextRow;
             col = nextCol;
         }
-
-        num=siameseSequence(num, step);
+        // iterate forwards the step linearly
+        num = num + step;
     }
 
     return siameseMagicSquare;
 }
 
-
 // Function to create the Strachey magic square
-vector<vector<int>> createStracheyMagicSquare(int size, int step)
+vector<vector<unsigned long long>> generateStracheyMagicSquare(unsigned long long size, unsigned long long step)
 {
     // Step 1: Divide the grid into 4 quarters
-    vector<vector<int>> magicSquare(size, vector<int>(size, 0));
+    vector<vector<unsigned long long>> magicSquare(size, vector<unsigned long long>(size, 0));
 
     // Step 2: Create Siamese magic squares for each quarter
-    vector<vector<int>> subSquareA = generateSiameseMagicSquare(1, size/2, step);
-    vector<vector<int>> subSquareB = generateSiameseMagicSquare(size * size / 4 + 1, size/2, step);
-    vector<vector<int>> subSquareC = generateSiameseMagicSquare(size * size / 2 + 1, size/2, step);
-    vector<vector<int>> subSquareD = generateSiameseMagicSquare(3 * size * size / 4 + 1, size/2, step);
-    
+    vector<vector<unsigned long long>> subSquareA = generateSiameseMagicSquare(1, size / 2, step);
+    vector<vector<unsigned long long>> subSquareB = generateSiameseMagicSquare(size * size / 4 + 1, size / 2, step);
+    vector<vector<unsigned long long>> subSquareC = generateSiameseMagicSquare(size * size / 2 + 1, size / 2, step);
+    vector<vector<unsigned long long>> subSquareD = generateSiameseMagicSquare(3 * size * size / 4 + 1, size / 2, step);
+
     // k is now equal to (size - 2) / 4
     // Step 3: Exchange the leftmost k columns in sub-square A with the corresponding columns of sub-square D.
-    for(int i = 0; i<size/2; i++) {
-        for(int j = 0; j<(size - 2) / 4; j++) {
-            swap(subSquareA[i][j],subSquareD[i][j]);
+    for (unsigned long long i = 0; i < size / 2; i++)
+    {
+        for (unsigned long long j = 0; j < (size - 2) / 4; j++)
+        {
+            swap(subSquareA[i][j], subSquareD[i][j]);
         }
     }
 
     // Step 4: Exchange the rightmost k - 1 columns in sub-square C with the corresponding columns of sub-square B.
-    for(int i = 0; i<size/2; i++) {
-        for(int j = size/2 - ((size - 2) / 4)+1; j<size/2; j++) {
-            swap(subSquareB[i][j],subSquareC[i][j]);
+    for (unsigned long long i = 0; i < size / 2; i++)
+    {
+        for (unsigned long long j = size / 2 - ((size - 2) / 4) + 1; j < size / 2; j++)
+        {
+            swap(subSquareB[i][j], subSquareC[i][j]);
         }
     }
     // Step 5: Exchange the middle cell of the leftmost column of sub-square A with the corresponding cell of sub-square D.
     swap(subSquareA[size / 4][0], subSquareD[size / 4][0]);
     // Exchange the central cell in sub-square A with the corresponding cell of sub-square D.
     swap(subSquareA[size / 4][size / 4], subSquareD[size / 4][size / 4]);
-    
+
     // Combine the sub-squares into the final magic square
-    for (int i = 0; i < size / 2; ++i) {
-        for (int j = 0; j < size / 2; ++j) {
+    for (unsigned long long i = 0; i < size / 2; ++i)
+    {
+        for (unsigned long long j = 0; j < size / 2; ++j)
+        {
             // Sub-square A (top left)
             magicSquare[i][j] = subSquareA[i][j];
             // Sub-square B (bottom right)
@@ -154,16 +158,114 @@ vector<vector<int>> createStracheyMagicSquare(int size, int step)
     return magicSquare;
 }
 
+vector<vector<unsigned long long>> generateDoublyEvenMagicSquare(unsigned long long size)
+{
+    vector<vector<unsigned long long>> magicSquare(size, vector<unsigned long long>(size, 0));
+    unsigned long long iterator = 1;
+    for (unsigned long long i = 0; i < size; ++i)
+    {
+        for (unsigned long long j = 0; j < size; ++j, ++iterator)
+        {
+            magicSquare[i][j] = (((i + 1) / 2) + ((j + 1) / 2) + 1) % 2 == 1 ? iterator : (size * size) + 1 - iterator;
+            /*
+            if((((i+1)/2)+((j+1)/2)+1)%2==1) { //to see where this monstrosity comes from, check out dea.cpp
+                magicSquare[i][j]=iterator;
+            } else { //it would be silly to make this a ternary operator statement. oops my finger slipped.
+                magicSquare[i][j]=(size*size)+1-iterator;
+            }
+            */
+            // iterator++; //also works without this.  oops i did another silly and added the iterator in the magicsquare input
+            // is there another linear iterator we can use such as step?
+            // no, because it subtracts from n^2 and becomes negative.
+            // could there be a max subtract instead to make a range?
+            // purposefully keeping the dead code because A. its more readable and B. it will be easier to implement from for a step feature
+        }
+    }
+    return magicSquare;
+}
+
+void writeMagicSquareToCSV(const std::vector<std::vector<unsigned long long>> &square, const std::string &filename)
+{
+    std::ofstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return;
+    }
+
+    unsigned long long n = square.size();
+    for (unsigned long long i = 0; i < n; ++i)
+    {
+        unsigned long long rowSum = 0;
+        for (unsigned long long j = 0; j < n; ++j)
+        {
+            rowSum += square[i][j];
+            file << square[i][j] << ",";
+        }
+        file << "," << rowSum << std::endl;
+    }
+
+    for(unsigned long long j = 0; j<=n; j++) {
+        file<<",";
+    }
+    file<<endl;
+
+    for (unsigned long long j = 0; j < n; ++j)
+    {
+        unsigned long long colSum = 0;
+        for (unsigned long long i = 0; i < n; ++i)
+        {
+            colSum += square[i][j];
+        }
+        file << colSum;
+        file << ",";
+    }
+
+    unsigned long long diagonalSum = 0;
+    unsigned long long antiDiagonalSum = 0;
+    for (unsigned long long i = 0; i < n; ++i)
+    {
+        diagonalSum += square[i][i];
+        antiDiagonalSum += square[i][n - i - 1];
+    }
+    file << ",\\" <<diagonalSum << " /" << antiDiagonalSum << std::endl;
+
+    cout << "Written magic square to " << filename << endl;
+    file.close();
+}
+
 int main()
 {
-    cout << "Strachey size: " << 4 * k + 2 << endl;
-    vector<vector<int>> magicSquare = createStracheyMagicSquare((4 * k) + 2, j);
-    vector<vector<int>> magicSiamese = generateSiameseMagicSquare(s, n, d);
+    unsigned long long size = 0;
+    cout << "What size magic square would you like?\n";
+    cin >> size;
 
-    cout << "The generated magic square:" << endl;
-    printMagicSquare(magicSquare, (4 * k) + 2);
+    vector<vector<unsigned long long>> magicSquare;
+    if (size % 2 == 1)
+    { // odd
+        magicSquare = generateSiameseMagicSquare(s, size, d);
+    }
+    else if (size % 4 == 2 && size >= 6)
+    { // singly even
+        magicSquare = generateStracheyMagicSquare(size, j);
+    }
+    else
+    { // doubly even
+        magicSquare = generateDoublyEvenMagicSquare(size);
+    }
+    //printMagicSquare(magicSquare, size);
+    writeMagicSquareToCSV(magicSquare, "test.csv");
 
-    cout << "The generated siamese square:" << endl;
-    printMagicSquare(magicSiamese, n);
+    /*
+        cout << "Strachey size: " << 4 * k + 2 << endl;
+        vector<vector<unsigned long long>> magicSquare = generateStracheyMagicSquare((4 * k) + 2, j);
+        vector<vector<unsigned long long>> magicSiamese = generateSiameseMagicSquare(s, n, d);
+
+        cout << "The generated magic square:" << endl;
+        printMagicSquare(magicSquare, (4 * k) + 2);
+
+        cout << "The generated siamese square:" << endl;
+        printMagicSquare(magicSiamese, n);
+        */
     return 0;
 }
